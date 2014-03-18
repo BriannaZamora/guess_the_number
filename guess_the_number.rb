a1df97849abb4451bdc4bc840e82733edd920504
BriannaZamora <bzamora@icloud.com>
2014-03-18 18:07:41 +0000

    checked guess and secret number

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,3 +1,10 @@
-puts "welcom to guess the name"
+puts "welcome to guess the name"
 secret_number = rand(6736590438)
-puts secret_number
+
+puts "Guess a number between 0 and 1000:"
+guess = gets.chomp.to_i
+if secret_number == to_i 
+ puts "CONGRADULATIONS!!! you got it right" 
+else
+  puts "WRONG! #{secret_number} is the answer"
+end
