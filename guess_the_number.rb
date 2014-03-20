4e83bcf14c897b8757c8993923bca320fb2d03df
BriannaZamora <bzamora@icloud.com>
2014-03-20 17:50:44 +0000

    I tried to put hints but for some reason couldnt get it to work hey i tried c:

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,13 +1,18 @@
 puts "welcome to guess the number"
-secret_number = rand(6)
+secret_number = rand(679)
 
 puts "Guess a number between 0 and 100:"
 guess = gets.chomp.to_i
 
-until guess == secret_number
+if guess == secret_number
     puts "wrong try again"
    guess = gets.chomp.to_i
+
+ until guess == secret_number 
+    puts "your close,try again"
+     guess = gets.chop.to_i
+  end 
 end
-
-puts "congratulations u got it right c:"
-
+if guess == secret_number
+    puts "congratulations u got it right c:"
+end
